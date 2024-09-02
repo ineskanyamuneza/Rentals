682bc53656e9aa34ea2c9e10706289194603e63d
Tumuhimbise-luck <luckyrazor11@gmail.com>
2024-09-02 16:27:20 +0300

    all

--- Admin/add-address.php
+++ Admin/add-address.php
@@ -10,8 +10,40 @@
         <div class="card">
          <center>
          <form action="" method="post">
+            <?php 
+            if(isset($_POST['add'])){
+            $address=$_POST['address'];
+            $select=mysqli_query($conn,"select * from tbl_address where address='$address'");
+            if($row=mysqli_fetch_array($select)){
+                ?>
+                <div class="alert">
+                    <h4>Oops! <?php echo $address; ?> is already there!</h4>
+                </div>
+                
+                <?php
+            }else{
+                $insert=mysqli_query($conn,"insert into tbl_address(a_id,address) values(null,'$address')");
+                if($insert){
+                    ?>
+                    <div class="success">
+                        <h4>Congs <?php echo $address; ?> added successfuly!</h4>
+                    </div>
+                    
+                    <?php
+                }else{
+                    ?>
+                    <div class="alert">
+                        <h4>Oops! <?php echo $address; ?> failed to insert...!</h4>
+                    </div>
+                    
+                    <?php
+                }
+
+            }
+                        }
+            ?>
             <input type="text" name="address" id="" autofocus placeholder="Enter Address" required>
-            <input type="submit" value="Add" class="btn">
+            <input type="submit" value="Add" name="add" class="btn">
 
          </form>
          </center>
