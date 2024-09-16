6f70d8e92090b225e1de6d2751fb20837c1c1c74
Tumuhimbise-luck <luckyrazor11@gmail.com>
2024-09-16 17:23:12 +0300

    all

--- landlord-signup.php
+++ landlord-signup.php
@@ -8,17 +8,20 @@
             <input type="text" name="name" id="" autofocus placeholder="Enter Full Name" required title="Enter your full name">
             <input type="text" name="contact" placeholder="Ex: 0787293050">
             <input type="email" name="email" placeholder="example@gmial.com">
+            <div class="radio">
+                <input type="radio" name="gender" value="1" id=""><label for="gender">Male</label> <input type="radio" value="Female" name="gender" id="">Female
+            </div>
+            <label for="dp" class="lable">Profile Picture</label>
             <input type="file" name="dp" id="imageUpload" accept="image/*" placeholder="Choose Your Profile">
+            <label for="document" class="lable">Upload Your <span style="color:red; font-style:italic;">pdf</span> file that contains National ID, LC 1's Letter </label>
             <input type="file" name="document" placeholder="Select your pdf document">
             <select name="address" id="">
                 <option value="">Select your Place of Residence</option>
-                <option value="">Kabalagala</option>
-                <option value="">Kampala</option>
-            </select>
+                <?php
+                $select=mysqli_query(); 
 
-            <div class="radio">
-                <input type="radio" name="gender" value="1" id="" checked><label for="gender">Male</label> <input type="radio" value="Female" name="gender" id="">Female
-            </div>
+                ?>
+            </select>
 
             <select name="nationality" id="">
                 <option value="">Select your Country of Origin </option>
